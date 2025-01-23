d40728ac90feb8b19bb8eeb1ccff553cf67a72b4
vlpsebas <117982629+vlpsebas@users.noreply.github.com>
2025-01-23 21:14:21 +0000

    added the code for function

--- functions/submit.js
+++ functions/submit.js
@@ -1,1 +1,25 @@
+export async function onRequestPost({ request, env }) {
+  
+  // Use this if the Worker is already handling the API
+  const workerUrl = "https://sunnylx.com/temps";
+
+  try {
+    const response = await fetch(workerUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: await request.text(),
+    });
+
+    const responseBody = await response.text();
+
+    return new Response(responseBody, {
+      status: response.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response("Error connecting to the Worker", { status: 500 });
+  }
+}
 
